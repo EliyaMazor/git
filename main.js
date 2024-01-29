01f0cabaf454bbce6ea323292a65cffeb3723ce7
Eliya Mazor <eliyahmazor@gmail.com>
2024-01-29 23:31:51 +0200

    Add max size to the ball with smooth transition

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var gBallSize = 100
 function onBallClick(elBall) {
   gBallSize += 50
   
+  if(gBallSize > 400) gBallSize = 100
+
   elBall.style.width = `${gBallSize}px`
   elBall.style.height = `${gBallSize}px`
   elBall.innerText = gBallSize
