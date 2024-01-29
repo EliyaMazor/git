b2c8b45a28348de441f13e9e00a320783237ad53
Eliya Mazor <eliyahmazor@gmail.com>
2024-01-30 00:36:08 +0200

    Make different max diameter to each ball

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 var gBallSize = 100
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
   gBallSize += getRandomInt(20, 60)
 
-  if (gBallSize > 400) gBallSize = 100
+  if (gBallSize > maxDiameter) gBallSize = 100
 
   elBall.style.backgroundColor = getRandomColor()
   elBall.style.width = `${gBallSize}px`
